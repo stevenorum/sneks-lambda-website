381df36cae1e4d4a6657975bb33fee30e19cfcb9
Steve Norum <stevenorum@gmail.com>
2019-01-10 18:53:58 -0500

    Small fixes.

--- src/handlers.py
+++ src/handlers.py
@@ -17,7 +17,7 @@ class sblob(blob):
 def get_post(text, title="The story of the forgotten post title.", url="#", active=False, published_date=None):
     published_date = published_date if published_date else datetime(year=randint(2015,2018), month=randint(1,12), day=randint(1,28), hour=randint(0,23), minute=randint(0,60))
     return blob(
-        text=text,
+        text="<p>{}</p>".format(text),
         title=sblob(title, html=title),
         url=url,
         active=active,
@@ -30,13 +30,18 @@ def index_handler(*args, **kwargs):
         get_post("I can't remember what I was going to write about today.  Sorry.","This is a post title"),
         get_post("Yet again, I can't remember what I was going to write about today.  Sorry.","This is a second post title"),
         get_post("As per usual, I can't remember what I was going to write about today.  Sorry.","This is a third post title"),
+        get_post("As per usual, I can't remember what I was going to write about today.  Sorry.","This is a fourth post title"),
+        get_post("As per usual, I can't remember what I was going to write about today.  Sorry.","This is a fifth post title"),
+        get_post("As per usual, I can't remember what I was going to write about today.  Sorry.","This is a sixth post title"),
+        get_post("As per usual, I can't remember what I was going to write about today.  Sorry.","This is a seventh post title"),
     ]
+    posts.sort(reverse=True, key=lambda x:x.published_date)
     data = {
         "archive_title":None,
         "archive_text":None,
         "posts":posts,
         "post":posts[0],
-        "intended_template":"default",
+        "intended_template":"post",
         "menu":[],
         "rss_feed":None
     }
